56fa8a12f835ea032597f15d063695fdfc9c33e4
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-07-09 07:54:28 -0600

    File reorg, add Google and credentials as providers.

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,30 @@
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username:", type: "text", placeholder: "your-username" },
+        password: { label: "Password:", type: "password", placeholder: "your-password" }
+      },
+      async authorize(credentials) {
+        // temp hard-coded user for local development; no DB at this point
+        const user = { id: "1", name: "Jon Smith", email: "jsmith@email.com", password: "password" }
+
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+          return user
+        } else {
+          return null
+        }
+      }
+    })
+  ],
+
+  pages: {}
+}
